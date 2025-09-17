8d6edb8bdbdd85ab13d57e9369a23ee4b3168192
EnzoBizeli <enzo.bizeli@sptech.school>
2025-09-16 22:40:23 -0300

    Atualização de CSV de Meteorologia

--- python/Base-Meteorologia/Base_Dados_Meteorologia.py
+++ python/Base-Meteorologia/Base_Dados_Meteorologia.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import unicodedata
 
-file_path = "INMET_SE_SP_A701_SAO PAULO - MIRANTE_01-01-2025_A_31-07-2025.CSV"
+file_path = "INMET_SE_SP_A701_SAO PAULO - MIRANTE_01-01-2025_A_31-08-2025.CSV"
 
 # Ler CSV tirando as primeiras 8 linhas de cabeçalho
-df = pd.read_csv(file_path, sep=";", encoding="utf-8", skiprows=8)
+df = pd.read_csv(file_path, sep=";", encoding="latin1", skiprows=8)
 
 df.columns = [
     unicodedata.normalize("NFKD", col)
@@ -50,14 +50,16 @@ df["ano"] = df["datetime"].dt.year
 # Função para gerar resumo diário de um mês
 def resumo_diario(df_mes):
     df_daily = df_mes.groupby(df_mes["datetime"].dt.date).agg({
-        "chuva_mm": "sum" if "chuva_mm" in df_mes else "first",
-        "temp_c": ["min", "max", "mean"] if "temp_c" in df_mes else "first",
-        "umidade": ["min", "max", "mean"] if "umidade" in df_mes else "first"
+        "chuva_mm": ["min", "max", "mean", "sum"] if "chuva_mm" in df_mes else "first",
+        "temp_c": ["min", "max", "mean", "sum"] if "temp_c" in df_mes else "first",
+        "umidade": ["min", "max", "mean", "sum"] if "umidade" in df_mes else "first"
     }).reset_index().rename(columns={"datetime": "dia"})
     if "temp_c" in df_mes:
-        df_daily.columns = ['dia', 'chuva_mm', 
-                            'temp_c_min', 'temp_c_max', 'temp_c_mean', 
-                            'umidade_min', 'umidade_max', 'umidade_mean']
+        df_daily.columns = [
+            'dia',
+            'chuva_mm_min', 'chuva_mm_max', 'chuva_mm_mean', 'chuva_mm_total',
+            'temp_c_min', 'temp_c_max', 'temp_c_mean', 'temp_c_total',
+            'umidade_min', 'umidade_max', 'umidade_mean', 'umidade_total']
     return df_daily
 
 # Resumo diário para cada mês disponível
